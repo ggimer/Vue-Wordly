a6197afcf7219f13eeacd37ebbad1b9d4f9f62aa
Grant Gimer <52215825+ggimer@users.noreply.github.com>
2022-05-15 19:53:54 -0500

    note about API not working
    05/15/2022 . . . The Dictionary API, used for checking guesses, seems to be not working.. If the game isn't working, it's because the API is down. Sorry about that. If this isn't resolved soon, I'll have to rewrite the game logic so it doesn't depend on that API.

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -5,5 +5,14 @@ import GameBoard from "@/components/GameBoard.vue";
 <template>
   <main>
     <GameBoard />
+
+    <br /><br /><br />
+    <div style="color: maroon">
+      05/15/2022 . . . The
+      <a href="https://dictionaryapi.dev">Dictionary API</a>, used for checking
+      guesses, seems to be not working.. If the game isn't working, it's because
+      the API is down. Sorry about that. If this isn't resolved soon, I'll have
+      to rewrite the game logic so it doesn't depend on that API.
+    </div>
   </main>
 </template>
